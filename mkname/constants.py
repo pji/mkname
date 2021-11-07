040a2fdabc284384ade881c03154577b9b5394a5
Paul J. Iutzi <pji@mac.com>
2021-11-07 10:58:52 -0600

    Moved to using pathlib for file locations.

--- mkname/constants.py
+++ mkname/constants.py
@@ -4,10 +4,17 @@ constants
 
 Default configuration values for mknames.
 """
-# Configuration file location.
-CONFIG_FILE = 'mkname/data/defaults.cfg'
-DEFAULT_CONFIG = 'mkname/data/defaults.cfg'
-DEFAULT_DB = 'mkname/data/names.db'
+from pathlib import Path
+
+
+# Path roots.
+PKG_ROOT = Path(__file__).parent
+DATA_ROOT = PKG_ROOT / 'data'
+
+# File locations.
+CONFIG_FILE = DATA_ROOT / 'defaults.cfg'
+DEFAULT_CONFIG = DATA_ROOT / 'defaults.cfg'
+DEFAULT_DB = DATA_ROOT / 'names.db'
 LOCAL_CONFIG = 'mkname.cfg'
 LOCAL_DB = 'names.db'
 
