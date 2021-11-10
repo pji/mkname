2582116dce108f005ed623cd767abc65d5bba555
Paul J. Iutzi <pji@mac.com>
2021-11-10 07:27:05 -0600

    Exposed module functions through __init__.

--- mkname/__init__.py
+++ mkname/__init__.py
@@ -4,8 +4,22 @@ __init__
 
 A Python module for creating names using other names as building blocks.
 """
-from mkname.mkname import build_compound_name, select_name
-from mkname.mod import add_scifi_letters, garble
-
-
-# Default values.
+from mkname.db import (
+    get_names,
+    get_names_by_kind
+)
+from mkname.mkname import (
+    get_config,
+    init_db,
+    build_compound_name,
+    build_from_syllables,
+    select_name
+)
+from mkname.mod import (
+    add_scifi_letters,
+    compound_names,
+    garble,
+    translate_characters,
+    make_scifi,
+    mods
+)
